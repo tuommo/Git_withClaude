fc9690605be1ad24808092ee070b34db32ef5c4d
Tomas Jezek <tuommo@gmail.com>
2026-09-18 11:39:50 +0200

    Add multiply function

--- kalkulacka.py
+++ kalkulacka.py
@@ -11,8 +11,11 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print("Kalkulacka v0.1")
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
+    print("3 * 3 =",multiply(3, 3))
