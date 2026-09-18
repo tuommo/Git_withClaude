386b2ab985e6d844801149a99e9d0e412061a9cb
Tomas Jezek <tuommo@gmail.com>
2026-09-18 11:45:18 +0200

    Add divide function and add raising error when dividing by 0

--- kalkulacka.py
+++ kalkulacka.py
@@ -14,8 +14,17 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Nelze dělit nulou")
+    return a / b
+
 if __name__ == "__main__":
     print("Kalkulacka v0.1")
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
     print("3 * 3 =",multiply(3, 3))
+    try:
+        print("10 / 0 =", divide(10, 0))
+    except ValueError as e:
+        print("Chyba:", e)
