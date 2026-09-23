56b16e9e84dd083ac328396af6be8e9e756ae008
Tomas Jezek <tuommo@gmail.com>
2026-09-23 09:42:47 +0200

    Update docstring about author/date.

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,6 +1,8 @@
 """
 Jednoducha kalkulacka - vychozi bod pro kurz Git & GitHub.
 Tento soubor se bude v prubehu kurzu postupne menit a verzovat.
+Cílem není mít funkční skript, který počítá, ale vyzkoušet si hlavně Git/GitHub verzování.
+Autor/Datum: Tomáš Ježek, 09/2026
 """
 
 
